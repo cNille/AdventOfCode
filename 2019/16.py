86d50935871a3897851eac8828f9a943ed11048b
Christopher Nilsson <c@shapeapp.se>
2022-12-05 06:56:01 +0100

    Clean 2019 day 16

--- 2019/16.py
+++ 2019/16.py
@@ -1,6 +1,5 @@
 data = open('16.input', 'r').readline().strip() 
 
-
 base = [ 0 , 1 , 0 , -1 ]
 signal = map(int, data)
 signal = [1,2,3,4,5,6,7,8]
@@ -17,9 +16,6 @@ def calc(signal, base):
         res += s * base[(pos+1) % len(base)] 
     return abs(res) % 10
 
-
-
-
 def fft(signal, base):
     new_signal = []
     for i in range(len(signal)):
@@ -28,15 +24,14 @@ def fft(signal, base):
         new_signal.append(res)
     return new_signal
 
-
 def transformer(signal, iterations):
     s = signal
-    for i in range(iterations):
+    for _ in range(iterations):
         s = fft(s, base) 
     return s
 
 def transformer_2(signal, iterations):
-    for i in range(iterations):
+    for _ in range(iterations):
         arr = []
         summing = sum(signal)
         arr.append(abs(summing % 10))
@@ -44,27 +39,21 @@ def transformer_2(signal, iterations):
         for s in signal: 
             summing -= s
             arr.append(abs(summing % 10))
-
         signal = arr
-
     return signal
 
-
-
-signal = map(int, '80871224585914546619083218645595') 
+# Test input
+# signal = [int(x) for x in '80871224585914546619083218645595']
 
 # Part 1
-signal = map(int, data)
+signal = [int(x) for x in data]
 res = transformer(signal, 100)
-res = "".join(map(str,res))
+res = "".join([str(x) for x in res])
 print("Part 1 result: %s" % res[:8])
 
-
-pos = 5976809
-
 # Part 2
-signal = map(int, data*10000)
+signal = [int(x) for x in data*10000]
+pos = int(data[:7])
 res = transformer_2(signal[pos:], 100)
-res = "".join(map(str,res))
+res = "".join([str(x) for x in res])
 print("Part 2 result: %s" % res[:8])
-

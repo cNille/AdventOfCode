41148359db478f9fa180154f954ff555fbd15e35
Christopher Nilsson <c@shapeapp.se>
2023-10-27 13:32:36 +0200

    Solution 2017 day 21

--- 2017/21_day/1_solution.py
+++ 2017/21_day/1_solution.py
@@ -1,55 +1,124 @@
-lines = open('my.input', 'r').read().split('\n')
+lines = open('./21_day/my.input', 'r').read().split('\n')
 
-image = [
-    ['.', '#', '.'],
-    ['.', '.', '#'],
-    ['#', '#', '#'],
+
+# Rotate key
+def rotate_key(key: list[str]):
+    if len(key) == 2:
+        return "/".join([
+            key[1][0] + key[0][0],
+            key[1][1] + key[0][1]
+        ])
+    if len(key) == 3:
+        return "/".join([
+            key[2][0] + key[1][0] + key[0][0],
+            key[2][1] + key[1][1] + key[0][1],
+            key[2][2] + key[1][2] + key[0][2],
+        ])
+    raise Exception('Unknown key size')
+
+
+# Flip key
+def flip_key(key: list[str]):
+    if len(key) == 2:
+        return "/".join([
+            key[1][0] + key[1][1],
+            key[0][0] + key[0][1]
+        ])
+    if len(key) == 3:
+        return "/".join([
+            key[2][0] + key[2][1] + key[2][2],
+            key[1][0] + key[1][1] + key[1][2],
+            key[0][0] + key[0][1] + key[0][2],
+        ])
+    raise Exception('Unknown key size')
+
+
+rules_list = [
+    "../.# => ##./#../...",
+    ".#./..#/### => #..#/..../..../#..#"
 ]
 
-def convert_to_int(x):
-    if x == '#':
-        return 1
-    return 0    
+rules_list = lines[:-1]
 
-def two_square_to_tuple(x):
-    return (
-        (x[0][0], x[0][1]),
-        (x[1][0],x[1][1])
-    )
-def three_square_to_tuple(x):
-    return (
-        (x[0][0], x[0][1], x[0][2]),
-        (x[1][0], x[1][1], x[1][2]),
-        (x[2][0], x[2][1], x[2][2]),
-    )
 
-def convert_2_square(image):
-    new_image = []
-    for x in range(len(image)/2):
-        new_row = []
-        for y in range(len(image)/2):
-            new_row.append([
-                image[x*2][y*2],
-                image[x*2][y*2+1],
-            ])
-        
+rules_list = [x.split(' => ') for x in rules_list]
+rules: dict[str, str] = {x[0]: x[1] for x in rules_list}
 
-enhancements = {}
-lines = [line for line in lines if len(line) > 0]
-for line in lines:
-    [inp, out] = line.split(' => ')
+extra_rules = {x: rules[x] for x in rules}
+for key in rules:
+    rotated_key = rotate_key(key.split('/'))
+    flipped_key = flip_key(key.split('/'))
+    rotated_flipped_key = rotate_key(flipped_key.split('/'))
+    extra_rules[flipped_key] = extra_rules[key]
+    extra_rules[rotated_flipped_key] = extra_rules[key]
+    rotated_flipped_key = rotate_key(rotated_flipped_key.split('/'))
+    extra_rules[rotated_flipped_key] = extra_rules[key]
+    rotated_flipped_key = rotate_key(rotated_flipped_key.split('/'))
+    extra_rules[rotated_flipped_key] = extra_rules[key]
+    rotated_flipped_key = rotate_key(rotated_flipped_key.split('/'))
+    extra_rules[rotated_flipped_key] = extra_rules[key]
+    extra_rules[rotated_key] = extra_rules[key]
+    rotated_key = rotate_key(rotated_key.split('/'))
+    extra_rules[rotated_key] = extra_rules[key]
+    rotated_key = rotate_key(rotated_key.split('/'))
+    extra_rules[rotated_key] = extra_rules[key]
 
-    inp = [list(x) for x in inp.split('/')]
-    out = [list(x) for x in out.split('/')]
+rules = extra_rules
+del extra_rules
 
-    if len(inp) == 2:
-        inp = two_square_to_tuple(inp)
-        #out = two_square_to_tuple(out)
-    else:    
-        inp = three_square_to_tuple(inp)
-        #out = three_square_to_tuple(out)
-    enhancements[inp] = out 
+# for r in rules:
+#    print(r, "\t=>\t", rules[r])
 
-for i in range(5):
+image = [
+    '.#.',
+    '..#',
+    '###',
+]
+
+for iteration in range(18):
+    print("Iteration", iteration)
     if len(image) % 2 == 0:
-        image = convert_2_square(image)
+        new_image = []
+        for y in range(0, len(image), 2):
+            new_image.append([])
+            for x in range(0, len(image), 2):
+                key = "/".join([
+                    image[y][x:x+2],
+                    image[y+1][x:x+2]
+                ])
+                new_image[-1].append(rules[key].split('/'))
+        image = []
+        for y in range(len(new_image)):
+            for i in range(len(new_image[y][0])):
+                image.append('')
+            for x in range(len(new_image[y])):
+                for i in range(len(new_image[y][x])):
+                    image[-1-i] += new_image[y][x][i]
+    elif len(image) % 3 == 0:
+        new_image = []
+        for y in range(0, len(image), 3):
+            new_image.append([])
+            for x in range(0, len(image), 3):
+                key = "/".join([
+                    image[y][x:x+3],
+                    image[y+1][x:x+3],
+                    image[y+2][x:x+3]
+                ])
+                new_image[-1].append(rules[key].split('/'))
+        image = []
+        for y in range(len(new_image)):
+            for i in range(len(new_image[y][0])):
+                image.append('')
+            for x in range(len(new_image[y])):
+                for i in range(len(new_image[y][x])):
+                    image[-1-i] += new_image[y][x][i]
+    else:
+        raise Exception('Unknown image size')
+
+    # print("\n".join(image))
+    # print()
+
+    print("Iteration", iteration, ":", sum([x.count('#') for x in image]))
+
+# Too high: 330
+# Next: 162

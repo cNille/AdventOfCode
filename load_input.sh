8deb1715905bfdf22b08490159eb7493852d984f
Christopher Nilsson <c@shapeapp.se>
2023-12-09 07:43:54 +0100

    Update load_input to have env-var and be in root

--- load_input.sh
+++ load_input.sh
@@ -1,14 +1,14 @@
+AOC_DIR=/Users/nille/aoc
 YEAR=`date '+%Y'`
 day=`date '+%d'`
 
-# Get cookie from adventofcode.com
-COOKIE=""
-
+# Source the .env file in the aoc directory
+source $AOC_DIR/.env
 
 sleep 5
 
 curl\
   --silent\
   -H "Cookie: session=$COOKIE"\
-  -o /Users/nille/aoc/$YEAR/$day.input\
+  -o $AOC_DIR/$YEAR/$day.input\
   https://adventofcode.com/$YEAR/day/${day#0}/input 

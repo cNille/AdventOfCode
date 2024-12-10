40c5c76666afe0fc3971129e74912cba8e6ff7a0
Christopher Nilsson <c@shapeapp.se>
2024-12-10 06:36:43 +0100

    Solve 2024 day 10

--- 2024/10.py
+++ 2024/10.py
@@ -1,62 +1,68 @@
 print(chr(27)+'[2j')
 print('\033c')
-f = open('10.test', 'r')
-f = open('10.test2', 'r')
-f = open('10.test3', 'r')
-f = open('10.test4', 'r')
+#f = open('10.test', 'r')
+#f = open('10.test2', 'r')
+#f = open('10.test3', 'r')
+#f = open('10.test4', 'r')
+#f = open('10.test5', 'r')
+#f = open('10.test6', 'r')
+#f = open('10.test7', 'r')
+#f = open('10.test8', 'r')
 f = open('10.test5', 'r')
-f = open('10.input', 'r')
+#f = open('10.input', 'r')
 lines = [x.strip() for x in f.readlines()]
 
 g = {}
 trailheads = []
 trailends = []
 for y, line in enumerate(lines):
-    print(line)
     for x, c in enumerate(line):
         g[(x,y)] = c
         if c == '0':
             trailheads.append((x,y))
         if c == '9':
             trailends.append((x,y))
-print(g)
-print(trailheads)
-print(trailends)
 
 D = [(0,1), (0,-1), (1,0), (-1,0)]
-def hike(g, pos, visited):
+def hike(g, pos, path):
     x,y = pos
     ends = set()
-    new_visited = set()
-    new_visited.update(visited)
-    new_visited.add(pos)
+    new_path = [p for p in path]
+    new_path.append(pos)
+    paths = []
     for dx,dy in D:
         x1 = x + dx
         y1 = y + dy
-        if (x1, y1) in visited: 
+        if (x1, y1) in new_path: 
             continue
         if (x1,y1) not in g:
             continue
         if g[(x1,y1)] == '.':
             continue
 
+        v  = int(g[(x,y)])
         v1 = int(g[(x1,y1)])
-        v =  int(g[(x,y)])
         if (v1-v) != 1:
             continue
 
         if v1 == 9:
+            paths.append(new_path) 
             ends.add((x1,y1))
             continue
         
-        ends.update(hike(g, (x1,y1), new_visited))
-    return ends
+        new_ends, p = hike(g, (x1,y1), new_path)
+        paths.extend(p)
+        ends.update(new_ends)
+    return ends, paths
 
 total_score = 0
+total_rating = 0
 for head in trailheads:
-    ends = hike(g, head, set()) 
+    ends, paths = hike(g, head, set()) 
     score = len(ends)
-    print('HEAD',head, '->', score)
     total_score += score
+    rate = len(paths)
+    total_rating += rate
 
-print('Total score:',total_score)
+print('Solution part1:',total_score)
+print('Solution part2:', total_rating)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2024 - 18 ⭐
+  2024 - 19 ⭐
 </h1>
 <a href="2024/01.ex">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -30,6 +30,9 @@
 </a>
 <a href="2024/09.py">
   <img src=".aoc_tiles/tiles/2024/09.png" width="161px">
+</a>
+<a href="2024/10.py">
+  <img src=".aoc_tiles/tiles/2024/10.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐
